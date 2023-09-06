613cde83468480f0432842894c87ab5d131a5447
GaleyDesign <christophergaleydesign@gmail.com>
2023-09-06 18:15:50 -0400

    worked account

--- account/views.py
+++ account/views.py
@@ -1,24 +1,51 @@
-from rest_framework.generics import CreateAPIView, APIView
+from rest_framework.generics import CreateAPIView, RetrieveAPIView
 from .models import UserProfile
 from rest_framework.response import Response
 from django.contrib.auth.models import User
-from .serializers import SignupSerializer
-from rest_framework.permissions import AllowAny
-from rest_framework.permissions import IsAuthenticated
-
+from .serializers import SignupSerializer, UserProfileSerializer
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 class SignupView(CreateAPIView):
-    queryset = User.objects.all()
     serializer_class = SignupSerializer
     permission_classes = [AllowAny]
 
     def perform_create(self, serializer):
         if serializer.is_valid():
-            email = serializer.validated_data["email"]
+            username = serializer.validated_data["username"]
             password = serializer.validated_data["password"]
-            User.objects.create_user(email=email, password=password)
+            User.objects.create_user(username=username, password=password)
             
-class ProfileView(APIView):
+class ProfileView(RetrieveAPIView):
+    serializer_class = UserProfileSerializer
     permission_classes = [IsAuthenticated]
-    def get(self, request, id):
-        profile = UserProfile.objects.get(id=id)
+   
+    def get_object(self):
+        try:  
+            user_profile = UserProfile.objects.get(user=self.request.user)
+            return user_profile
+        except UserProfile.DoesNotExist:  
+            return Response({'detail': 'Profile not found.'})
+        
+class ProfileCreate(CreateAPIView):
+    serializer_class = UserProfileSerializer
+    permission_classes = [IsAuthenticated]
+    
+    def create(self, request):
+        existing_profile_check = UserProfile.objects.filter(user=self.request.user).first()
+        if existing_profile_check:
+            return Response({'detail': 'Profile already exists.'})
+
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(
+                user=self.request.user,
+                street_name=request.data['street_name'],
+                street_number=request.data['street_number'],
+                zip_code=request.data['zip_code'],
+                city=request.data['city']
+            )
+
+            return Response({'detail': 'Profile created successfully.'})
+    
+
+            
